93e39e1cf15a695dd0c6f0df86ba30f747cb9ae5
Balaji K <balajik@Balajis-MacBook-Air.local>
2026-05-05 12:39:45 +0530

    fix: Add country code to mobile_no

--- yoga_meditation/setup_demo_data.py
+++ yoga_meditation/setup_demo_data.py
@@ -65,7 +65,7 @@ def create_students():
                 "doctype": "Student Member",
                 "full_name": s["name"],
                 "email_id": s["email"],
-                "mobile_no": s.get("phone", "9876543210"),
+                "mobile_no": s.get("phone", "+919876543210"),
                 "membership_status": "Active"
             }).insert()
 
